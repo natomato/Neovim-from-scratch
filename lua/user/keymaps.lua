7456800ce5d6e07686af802905cada4e6a68609a
Nater <natomato@users.noreply.github.com>
2022-03-30 18:13:08 -0500

    removes jk keymap
    instead use capslock -> escape to exit insert mode

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -39,9 +39,7 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
--- Insert --
--- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
+-- Insert Mode --
 
 -- Visual --
 -- Stay in indent mode
